f0babeab0574049f4fd8bbd8c77e113e273116a5
luoyi <luoyi58624@gmail.com>
2023-06-10 17:00:02 +0800

    移除webview

--- lib/page/root/home_page.dart
+++ lib/page/root/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_material3/common/widget.dart';
-import 'package:flutter_material3/page/test/rich_text_editor.dart';
-import 'package:flutter_material3/page/test/webview.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,14 +16,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: ListView(
         children: [
-          ListTileWidget(
-            'webview测试',
-            page: const WebviewTestPage(),
-          ),
-          ListTileWidget(
-            '富文本编辑器测试',
-            page: const RichTextEditorPage(),
-          ),
+
         ],
       ),
     );
